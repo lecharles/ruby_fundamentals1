cee8fe81906ceeb76a40e8ec3cc7557c64f9cfd9
lecharles <charleslozano@gmail.com>
2016-01-12 15:00:38 -0500

    exercise2

---
+++ exercise2.rb
@@ -0,0 +1,15 @@
+# Ex 2.1 Calculating a good tip in Canada:
+# puts "How much was your meal?"
+# meal = gets.chomp.to_i
+# tip = meal * 0.20
+# puts "#{tip} is a good tip in Canadian soil"
+
+# Ex 2.2 Adding a string and an integer with the + operator:
+# puts "this is how you could add an integer such as " + 5.to_s + " to a string."
+
+# # Ex 2.3 result of 45628 multiplied by 7839 in a sentence by string interpolatº
+# puts ( 45628 * 7839 ).to_s
+
+# # Ex 2.4 value of the expression (10 < 20 && 30 < 20) || !(10 == 11)
+# !!((10 < 20 && 30 < 20) || !(10 == 11))
+# true #checked leveraging irb
